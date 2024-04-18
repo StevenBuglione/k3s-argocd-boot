a2d853e20cbff69649c2d13ba22c170dac3865fc
steve <stevenbuglione1@gmail.com>
2024-04-17 21:23:51 -0400

    Big Update Let's Hope Things Won't break

--- secret-gen.sh
+++ secret-gen.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Define your parameters
-SECRET_NAME=test-secret
-NAMESPACE=default
+SECRET_NAME=cloudflare-token-secret
+NAMESPACE=cert-manager
 UNSEALED_SECRET_OUTPUT=$SECRET_NAME.json
 SEALED_SECRET_OUTPUT=sealed-$SECRET_NAME.yaml
 
 # Define your secret data as an associative array
 declare -A SECRET_DATA=(
-    ["key1"]="value1"
-    ["key2"]="value2"
+    [""]=""
     # Add more key-value pairs as needed
 )
 
